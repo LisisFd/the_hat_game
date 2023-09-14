2a8cc6023f7a1a0c7664d258b3a047d437715993
LisisFd <lisis.fd@gmail.com>
2023-09-14 19:03:10 +0300

    refactor

--- tools/build-json.sh
+++ tools/build-json.sh
@@ -6,4 +6,4 @@ BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
 cd ../app/app_main
 flutter pub get
-flutter packages pub run build_runner build
+flutter packages pub run build_runner build --delete-conflicting-outputs
