a04d2b02a190df217072e68cea583e730b41c8c8
LisisFd <lisis.fd@gmail.com>
2023-09-16 13:16:53 +0300

    refactor

--- app/app_main/lib/view/game_set_up/teams/teams_screen.dart
+++ app/app_main/lib/view/game_set_up/teams/teams_screen.dart
@@ -1,5 +1,6 @@
 import 'package:app_core/app_core.dart';
 import 'package:core_flutter/core_flutter.dart';
+import 'package:core_ui/core_ui.dart';
 
 class TeamsScreenArguments {}
 
@@ -19,7 +20,10 @@ class _TeamsScreenState extends State<TeamsScreen> {
   static const int _minTeamCount = 2;
   static const int _minPlayers = 4;
   static const int _maxPlayers = 10;
+  final _listTeamsKey = GlobalKey<AnimatedListState>();
+
   int _totalPlayers = _minPlayers;
+
   final List<String> _teams = [
     'First',
     'Second',
@@ -41,133 +45,115 @@ class _TeamsScreenState extends State<TeamsScreen> {
     '3',
     '5',
   ];
-  late final List<String> _currentTeams = _teams.sublist(0, 3);
+  late final List<String> _currentTeams = _teams.sublist(0, _minTeamCount);
 
-  List<Widget> _genTeamLine() {
-    List<Widget> teams = [];
-    for (String i in _currentTeams) {
-      teams.add(Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            i,
-            style: TextStyle(fontSize: 30),
-          ),
-          if (_currentTeams.indexOf(i) >= 2)
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.clear),
-            ),
-        ],
-      ));
+  void _addItem() {
+    int length = _currentTeams.length;
+    if (length < _teams.length) {
+      setState(() {
+        _currentTeams.add(_teams[length]);
+        _listTeamsKey.currentState?.insertItem(length);
+      });
     }
-    teams.add(
-      GestureDetector(
-        onTap: () {
-          int length = _currentTeams.length;
-          if (length < _teams.length) {
-            setState(() {
-              _currentTeams.add(_teams[length]);
-            });
-          }
-        },
-        child: const SizedBox(
-          width: double.infinity,
-          height: 40,
-          child: ColoredBox(
-            color: Colors.red,
+  }
+
+  void _removeItem(int index) {
+    setState(() {
+      _listTeamsKey.currentState
+          ?.removeItem(index, (context, animation) => const SizedBox());
+      _currentTeams.removeAt(index);
+    });
+  }
+
+  Widget _getRow(int index) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              _currentTeams[index],
+              style: const TextStyle(fontSize: 30),
+            ),
+            if (_currentTeams.length > 2)
+              IconButton(
+                onPressed: () {
+                  _removeItem(index);
+                },
+                icon: const Icon(Icons.clear),
+              ),
+          ],
+        ),
+        if (_currentTeams.length - index == 1)
+          ElevatedButton(
+            onPressed: _addItem,
             child: const Icon(Icons.add),
           ),
-        ),
-      ),
+      ],
     );
-    return teams;
   }
 
 //TODO: add localization
   @override
   Widget build(BuildContext context) {
     return MyAppWrap(
-        body: Column(
-      children: [
-        const Text(
-          'Teams',
-          textAlign: TextAlign.center,
-        ),
-        Expanded(
-          child: ListView(
-            physics: const PositionRetainedScrollPhysics(),
-            children: _genTeamLine(),
+        body: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Column(
+        children: [
+          const Text(
+            'Teams',
+            textAlign: TextAlign.center,
           ),
-        ),
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text('Count players'),
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {
-                    if (_totalPlayers > _minPlayers) {
-                      setState(() {
-                        _totalPlayers--;
-                      });
-                    }
-                  },
-                  icon: const Icon(Icons.remove),
-                ),
-                Text(_totalPlayers.toString()),
-                IconButton(
-                  onPressed: () {
-                    if (_totalPlayers < _maxPlayers) {
-                      setState(() {
-                        _totalPlayers++;
-                      });
-                    }
-                  },
-                  icon: const Icon(Icons.add),
-                ),
-              ],
+          Expanded(
+            child: AnimatedList(
+              key: _listTeamsKey,
+              initialItemCount: _currentTeams.length,
+              physics: CoreScrollPhysics.positionRetained,
+              itemBuilder: (BuildContext context, int index,
+                  Animation<double> animation) {
+                return _getRow(index);
+              },
             ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Next'),
-            ),
-          ],
-        ),
-      ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text('Count players'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      if (_totalPlayers > _minPlayers) {
+                        setState(() {
+                          _totalPlayers--;
+                        });
+                      }
+                    },
+                    icon: const Icon(Icons.remove),
+                  ),
+                  Text(_totalPlayers.toString()),
+                  IconButton(
+                    onPressed: () {
+                      if (_totalPlayers < _maxPlayers) {
+                        setState(() {
+                          _totalPlayers++;
+                        });
+                      }
+                    },
+                    icon: const Icon(Icons.add),
+                  ),
+                ],
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
     ));
-  }
-}
-
-class PositionRetainedScrollPhysics extends ScrollPhysics {
-  final bool shouldRetain;
-
-  const PositionRetainedScrollPhysics({super.parent, this.shouldRetain = true});
-
-  @override
-  PositionRetainedScrollPhysics applyTo(ScrollPhysics? ancestor) {
-    return PositionRetainedScrollPhysics(
-      parent: buildParent(ancestor),
-      shouldRetain: shouldRetain,
-    );
-  }
-
-  @override
-  double adjustPositionForNewDimensions({
-    required ScrollMetrics oldPosition,
-    required ScrollMetrics newPosition,
-    required bool isScrolling,
-    required double velocity,
-  }) {
-    final position = super.adjustPositionForNewDimensions(
-      oldPosition: oldPosition,
-      newPosition: newPosition,
-      isScrolling: isScrolling,
-      velocity: velocity,
-    );
-
-    final diff = newPosition.maxScrollExtent - oldPosition.maxScrollExtent;
-    return position + diff;
   }
 }
